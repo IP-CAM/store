e601fb049fb16cc50a69897bf97c117f9accab53
Pedro Cones <pedrocones@yahoo.com>
2016-01-17 08:31:59 -0700

    few files store is running

---
+++ upload/config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://test/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://test/');
+
+// DIR
+define('DIR_APPLICATION', '/home/pedrocones/webmaster/Repository/store/upload/catalog/');
+define('DIR_SYSTEM', '/home/pedrocones/webmaster/Repository/store/upload/system/');
+define('DIR_LANGUAGE', '/home/pedrocones/webmaster/Repository/store/upload/catalog/language/');
+define('DIR_TEMPLATE', '/home/pedrocones/webmaster/Repository/store/upload/catalog/view/theme/');
+define('DIR_CONFIG', '/home/pedrocones/webmaster/Repository/store/upload/system/config/');
+define('DIR_IMAGE', '/home/pedrocones/webmaster/Repository/store/upload/image/');
+define('DIR_CACHE', '/home/pedrocones/webmaster/Repository/store/upload/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/pedrocones/webmaster/Repository/store/upload/system/storage/download/');
+define('DIR_LOGS', '/home/pedrocones/webmaster/Repository/store/upload/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/pedrocones/webmaster/Repository/store/upload/system/storage/modification/');
+define('DIR_UPLOAD', '/home/pedrocones/webmaster/Repository/store/upload/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'venezol1_HTCUSER');
+define('DB_PASSWORD', 'htc867502');
+define('DB_DATABASE', 'venezol1_store20160117');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');

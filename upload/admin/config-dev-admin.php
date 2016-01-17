939532443d8cfcf7fa6a7aaa85b31c40b5cdcadf
Pedro Cones <pedrocones@yahoo.com>
2016-01-17 08:11:02 -0700

    adding permission and few files images plus config

---
+++ upload/admin/config-dev-admin.php
@@ -0,0 +1,32 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://htc/store/upload/admin/');
+define('HTTP_CATALOG', 'http://htc/store/upload/');
+
+
+// HTTPS
+define('HTTPS_SERVER', 'http://htc/store/upload/admin/');
+define('HTTPS_CATALOG', 'http://htc/store/upload/');
+
+// DIR
+define('DIR_APPLICATION', '/home/pedrocones/webmaster/developer/hydrotools/store/upload/admin/');
+define('DIR_SYSTEM', '/home/pedrocones/webmaster/developer/hydrotools/store/upload/system/');
+define('DIR_IMAGE', '/home/pedrocones/webmaster/developer/hydrotools/store/upload/image/');
+define('DIR_LANGUAGE', '/home/pedrocones/webmaster/developer/hydrotools/store/upload/admin/language/');
+define('DIR_TEMPLATE', '/home/pedrocones/webmaster/developer/hydrotools/store/upload/admin/view/template/');
+define('DIR_CONFIG', '/home/pedrocones/webmaster/developer/hydrotools/store/upload/system/config/');
+define('DIR_CACHE', '/home/pedrocones/webmaster/developer/hydrotools/store/upload/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/pedrocones/webmaster/developer/hydrotools/store/upload/system/storage/download/');
+define('DIR_LOGS', '/home/pedrocones/webmaster/developer/hydrotools/store/upload/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/pedrocones/webmaster/developer/hydrotools/store/upload/system/storage/modification/');
+define('DIR_UPLOAD', '/home/pedrocones/webmaster/developer/hydrotools/store/upload/system/storage/upload/');
+define('DIR_CATALOG', '/home/pedrocones/webmaster/developer/hydrotools/store/upload/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'venezol1_HTCUSER');
+define('DB_PASSWORD', 'htc867502');
+define('DB_DATABASE', 'venezol1_store20160113');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');

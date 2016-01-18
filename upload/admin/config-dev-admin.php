42770575deb7cf553e3b561198f2b2a658f19fa9
Pedro Cones <pedrocones@yahoo.com>
2016-01-17 18:37:41 -0700

    clean up config-host

--- upload/admin/config-dev-admin.php
+++ upload/admin/config-dev-admin.php
@@ -1,32 +1,31 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://htc/store/upload/admin/');
-define('HTTP_CATALOG', 'http://htc/store/upload/');
-
+define('HTTP_SERVER', 'http://test/admin/');
+define('HTTP_CATALOG', 'http://test/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://htc/store/upload/admin/');
-define('HTTPS_CATALOG', 'http://htc/store/upload/');
+define('HTTPS_SERVER', 'http://test/admin/');
+define('HTTPS_CATALOG', 'http://test/');
 
 // DIR
-define('DIR_APPLICATION', '/home/pedrocones/webmaster/developer/hydrotools/store/upload/admin/');
-define('DIR_SYSTEM', '/home/pedrocones/webmaster/developer/hydrotools/store/upload/system/');
-define('DIR_IMAGE', '/home/pedrocones/webmaster/developer/hydrotools/store/upload/image/');
-define('DIR_LANGUAGE', '/home/pedrocones/webmaster/developer/hydrotools/store/upload/admin/language/');
-define('DIR_TEMPLATE', '/home/pedrocones/webmaster/developer/hydrotools/store/upload/admin/view/template/');
-define('DIR_CONFIG', '/home/pedrocones/webmaster/developer/hydrotools/store/upload/system/config/');
-define('DIR_CACHE', '/home/pedrocones/webmaster/developer/hydrotools/store/upload/system/storage/cache/');
-define('DIR_DOWNLOAD', '/home/pedrocones/webmaster/developer/hydrotools/store/upload/system/storage/download/');
-define('DIR_LOGS', '/home/pedrocones/webmaster/developer/hydrotools/store/upload/system/storage/logs/');
-define('DIR_MODIFICATION', '/home/pedrocones/webmaster/developer/hydrotools/store/upload/system/storage/modification/');
-define('DIR_UPLOAD', '/home/pedrocones/webmaster/developer/hydrotools/store/upload/system/storage/upload/');
-define('DIR_CATALOG', '/home/pedrocones/webmaster/developer/hydrotools/store/upload/catalog/');
+define('DIR_APPLICATION', '/home/pedrocones/webmaster/Repository/store/upload/admin/');
+define('DIR_SYSTEM', '/home/pedrocones/webmaster/Repository/store/upload/system/');
+define('DIR_LANGUAGE', '/home/pedrocones/webmaster/Repository/store/upload/admin/language/');
+define('DIR_TEMPLATE', '/home/pedrocones/webmaster/Repository/store/upload/admin/view/template/');
+define('DIR_CONFIG', '/home/pedrocones/webmaster/Repository/store/upload/system/config/');
+define('DIR_IMAGE', '/home/pedrocones/webmaster/Repository/store/upload/image/');
+define('DIR_CACHE', '/home/pedrocones/webmaster/Repository/store/upload/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/pedrocones/webmaster/Repository/store/upload/system/storage/download/');
+define('DIR_LOGS', '/home/pedrocones/webmaster/Repository/store/upload/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/pedrocones/webmaster/Repository/store/upload/system/storage/modification/');
+define('DIR_UPLOAD', '/home/pedrocones/webmaster/Repository/store/upload/system/storage/upload/');
+define('DIR_CATALOG', '/home/pedrocones/webmaster/Repository/store/upload/catalog/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
 define('DB_USERNAME', 'venezol1_HTCUSER');
 define('DB_PASSWORD', 'htc867502');
-define('DB_DATABASE', 'venezol1_store20160113');
+define('DB_DATABASE', 'venezol1_store20160117');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
